46b32eabae54241a16e8ab8f5d14f5aebacfbb85
nodormu <60243248+nodormu@users.noreply.github.com>
2025-12-04 15:51:29 -0600

    Add files via upload

---
+++ 02_setup-chaincode-directory-and-pull-go-contract-github.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# configuration
+set -e   # exit on any error
+set -x   # print each command as it runs
+
+
+# main script
+# be sure this script is in your /path/to/your/fabric/scripts/fabric-samples and run there, then remove the script when done.
+echo "create chaincode directory"
+mkdir chaincode
+cd chaincode
+mkdir contract-tutorial
+cd contract-tutorial
+
+echo "pulling github for chaincode tutorials"
+go mod init github.com/hyperledger/fabric-samples/chaincode/contract-tutorial
+
+echo "setting up modules"
+go get -u github.com/hyperledger/fabric-contract-api-go
+
+cat <<'EOF' > simple-contract.go
+package main
+
+import (
+    "errors"
+    "fmt"
+
+    "github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+// SimpleContract contract for handling writing and reading from the world state
+type SimpleContract struct {
+    contractapi.Contract
+}
+
+// Create adds a new key with value to the world state
+func (sc *SimpleContract) Create(ctx contractapi.TransactionContextInterface, key string, value string) error {
+    existing, err := ctx.GetStub().GetState(key)
+
+    if err != nil {
+        return errors.New("Unable to interact with world state")
+    }
+
+    if existing != nil {
+        return fmt.Errorf("Cannot create world state pair with key %s. Already exists", key)
+    }
+
+    err = ctx.GetStub().PutState(key, []byte(value))
+
+    if err != nil {
+        return errors.New("Unable to interact with world state")
+    }
+
+    return nil
+}
+
+// Update changes the value with key in the world state
+func (sc *SimpleContract) Update(ctx contractapi.TransactionContextInterface, key string, value string) error {
+    existing, err := ctx.GetStub().GetState(key)
+
+    if err != nil {
+        return errors.New("Unable to interact with world state")
+    }
+
+    if existing == nil {
+        return fmt.Errorf("Cannot update world state pair with key %s. Does not exist", key)
+    }
+
+    err = ctx.GetStub().PutState(key, []byte(value))
+
+    if err != nil {
+        return errors.New("Unable to interact with world state")
+    }
+
+    return nil
+}
+
+// Read returns the value at key in the world state
+func (sc *SimpleContract) Read(ctx contractapi.TransactionContextInterface, key string) (string, error) {
+    existing, err := ctx.GetStub().GetState(key)
+
+    if err != nil {
+        return "", errors.New("Unable to interact with world state")
+    }
+
+    if existing == nil {
+        return "", fmt.Errorf("Cannot read world state pair with key %s. Does not exist", key)
+    }
+
+    return string(existing), nil
+}
+EOF
